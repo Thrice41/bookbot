9398f6a7dfcddad957844f355c3b0a27adf7b033
Thrice41 <Thrice41@gmail.com>
2025-07-24 21:58:46 -0400

    CH3: Report L1: Print a report
    PASS

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+#
+#
+#
+#
+
+# Sys module
+import sys
+# Functions from /bookbot/stats.py
 from stats import get_book_text, get_num_words, get_num_characters, sort_on, print_report
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def main():
 
-    book = "./books/frankenstein.txt"
+    book = sys.argv[1]
 
     book_text = get_book_text(book)
 
